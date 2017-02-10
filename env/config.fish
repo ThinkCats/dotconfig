89719767b97268b35f1c301bfee5bc3ba800a2c2
ThinkCat <624432528@qq.com>
2017-02-10 10:32:25 +0800

    add fish shell config

---
+++ env/config.fish
@@ -0,0 +1,12 @@
+set MAVEN_HOME $HOME/develop/apache-maven-3.3.9
+set PATH $PATH $MAVEN_HOME/bin /Users/hehe/develop/node-v6.2.2/bin
+
+# git alias
+alias gin 'git init'
+alias gia 'git add'
+alias gis 'git status'
+alias gic 'git commit -m'
+alias gpul 'git pull'
+alias gpus 'git push'
+
+
