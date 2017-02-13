b7696a55b22f6da6ddf69ac92e8ea11996c3bd6c
ThinkCat <624432528@qq.com>
2017-02-13 14:38:18 +0800

    add tmux config

--- env/config.fish
+++ env/config.fish
@@ -1,5 +1,9 @@
+
 set MAVEN_HOME $HOME/develop/apache-maven-3.3.9
-set PATH $PATH $MAVEN_HOME/bin /Users/hehe/develop/node-v6.2.2/bin
+set -x GOPATH /Users/hehe/workspace
+set NodePATH $HOME/develop/node-v6.2.2
+
+set PATH $PATH $MAVEN_HOME/bin  $GOPATH/bin $NodePATH/bin
 
 # git alias
 alias gin 'git init'
@@ -8,5 +12,8 @@ alias gis 'git status'
 alias gic 'git commit -m'
 alias gpul 'git pull'
 alias gpus 'git push'
+
+alias tx 'tmux'
+
 
 
